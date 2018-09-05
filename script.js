d84086ef0c8115e4041e9be955813455f50ed113
unknown <dcdeveloper26@gmail.com>
2018-09-04 21:36:39 -0700

    added update statement

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 //NOTE: this file is purely for testing the codebase.
 let _ba = require('./lib/batch-async.js');
 let records = require('./lib/records.js'),
-    peopleRecords = require('./lib/people.js');
+    peopleRecords = require('./lib/people.js'),
+    updatePeople= require('./lib/updateTest.js');
 
 //add the ability to let users input the properties they want parsed
 //from the json object, add normalization to the data, then insert it
 _ba({
   transactions: [
+    // {
+    //   batchQuery: true,
+    //   statementType: 'INSERT',
+    //   table: 'pet',
+    //   columns: ['name', 'owner', 'species', 'sex'],
+    //   values: records
+    // },
+    // {
+    //   batchQuery: true,
+    //   statementType: 'INSERT',
+    //   table: 'people',
+    //   columns: ['name', 'age', 'occupation', 'sex'],
+    //   values: peopleRecords
+    // },
     {
       batchQuery: true,
-      statementType: 'INSERT',
-      table: 'pet',
-      columns: ['name', 'owner', 'species', 'sex'],
-      values: records
-    },
-    {
-      batchQuery: true,
-      statementType: 'INSERT',
+      statementType: 'UPDATE',
       table: 'people',
       columns: ['name', 'age', 'occupation', 'sex'],
-      values: peopleRecords
+      values: updatePeople,
+      columnIdentifier: 'name'
     }
   ]
 }, function(err, response) {
