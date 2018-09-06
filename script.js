88fc5743903a7c7e8a564643af37201de3fded28
Dion Cavanaugh <29697144+DC-Developer@users.noreply.github.com>
2018-09-05 18:58:27 -0700

    Update statement (#2)
    * added query statement to handle update queries
    
    * joined ? array for sql
    
    * added update statement
    
    * Update script.js
    
    * took out batchQuery property for control flow, determined by value length instead
    
    * added custom field

--- script.js
+++ script.js
@@ -2,33 +2,45 @@
 let _ba = require('./lib/batch-async.js');
 let records = require('./lib/records.js'),
     peopleRecords = require('./lib/people.js'),
-    updatePeople= require('./lib/updateTest.js');
+    updatePeople= require('./lib/updateTest.js'),
+    customUpdate= require('./lib/customUpdate.js');
 
 //add the ability to let users input the properties they want parsed
 //from the json object, add normalization to the data, then insert it
 _ba({
   transactions: [
     {
-      batchQuery: true,
       statementType: 'INSERT',
       table: 'pet',
       columns: ['name', 'owner', 'species', 'sex'],
       values: records
     },
     {
-      batchQuery: true,
       statementType: 'INSERT',
       table: 'people',
       columns: ['name', 'age', 'occupation', 'sex'],
       values: peopleRecords
     },
     {
-      batchQuery: true,
       statementType: 'UPDATE',
       table: 'people',
       columns: ['name', 'age', 'occupation', 'sex'],
       values: updatePeople,
       columnIdentifier: 'name'
+    },
+    {
+      statementType: 'CUSTOM',
+      columns: ['Old Greggy', 'Comedian Actor'],
+      values: customUpdate,
+      customStatement: `
+        UPDATE people
+        SET
+        name = ?, occupation = ?
+        WHERE
+        name = ?
+        AND
+        occupation = ?
+        `
     }
   ]
 }, function(err, response) {
